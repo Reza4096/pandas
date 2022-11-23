52dfc3e01202e1fe37ed9f23dfe190b9fcd2fdcf
reza4096 <reza4096@yahoo.com>
2022-11-23 13:54:40 +0330

    The keys of the dictionary become the labels.

--- label.py
+++ label.py
@@ -7,3 +7,12 @@ myvar = pd.Series(a, index = ["x", "y", "z"])
 print(myvar)
 
 print(myvar["y"])
+
+#Key/Value Objects as Series
+#The keys of the dictionary become the labels.
+
+calories = {"day1": 420, "day2": 380, "day3": 390}
+
+myvar = pd.Series(calories)
+
+print(myvar)
