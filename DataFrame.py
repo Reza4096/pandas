85825b7734afe61cfe8f7216cbdd0d2a8cb3aa1f
reza4096 <reza4096@yahoo.com>
2022-11-23 13:59:58 +0330

    Locate Row in df

---
+++ DataFrame.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+#load data into a DataFrame object:
+df = pd.DataFrame(data)
+
+print(df)
+
+## Locate Row
+#refer to the row index:
+print(df.loc[0])
